8a8d71172207a0c36a7df2d41e3c05eb70936d23
Nihal Roshan <82870227+NihalRoshanCK@users.noreply.github.com>
2025-11-30 11:09:21 +0000

    update api

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-const baseURL = 'https://edusuite.store/';
+const baseURL = 'https://karate.edusoft.cloud/';
 const adminUrl = 'import.meta.env.VITE_ADMINBASEURL'
 
 // Axios instance for regular user
