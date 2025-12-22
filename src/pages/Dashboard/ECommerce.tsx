a7ce089a7d821c73ecbe9188340e641c8e4f8b35
Nihal Roshan <82870227+NihalRoshanCK@users.noreply.github.com>
2025-12-22 20:59:57 +0000

    update new event

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -69,7 +69,7 @@ console.log(user.user.id);
             <div ref={componentPDf} className="admindashboard-table">
               <div className="text-center justify-center flex p-5 bg-gradient-to-r from-red-500 to-black text-white text-2xl font-bold uppercase">
                 <p className="admindashboard-table-criteria">
-                  KERALA STATE KENYU RYU KARATE CHAMPIONSHIP
+                  SANKUKAI CUP SEASON 2 
                 </p>
               </div>
 
